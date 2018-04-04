443c29fbcfa8045cafcaedfa51ac0b9a5400546c
EC2 Default User <ec2-user@ip-172-31-28-205.us-east-2.compute.internal>
2018-04-04 22:54:58 +0000

    Server up and running

--- server/db/execdb.js
+++ server/db/execdb.js
@@ -1,22 +1,21 @@
-/*
- * setup.js -- Sets up the database.
- */
 'use strict'
-
 require('dotenv').config()
-const cp = require('child_process')
-const commandPassword = `mysql -u${process.env.DB_USER} -p${process.env.DB_PASS} < ${process.argv[2]}.sql`
-const commandNoPassword = `mysql -u${process.env.DB_USER} < ${process.argv[2]}.sql`
-cp.exec(process.env.DB_PASS === '' ? commandNoPassword : commandPassword,
-  function (error, stdout, stderr) {
-    if (error) {
-      console.log(error)
-      return
-    }
+var mysql = require('mysql');
 
-    if (process.argv[2] === 'setup') {
-      console.log('\x1b[32m%s\x1b[0m', 'DATABASES INITIALIZED')
-    } else if (process.argv[2] === 'teardown') {
-      console.log('\x1b[31m%s\x1b[0m', 'DATABASES DROPPED')
-    }
-  })
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  port     : process.env.DB_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
